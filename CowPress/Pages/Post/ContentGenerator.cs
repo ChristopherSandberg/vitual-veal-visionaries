322a12b7b68518699e6a3c557aabd300eb9927e7
Christopher Sandberg <christopher.sandberg@visma.com>
2023-11-21 13:06:57 +0100

    intial

--- CowPress/Pages/Post/ContentGenerator.cs
+++ CowPress/Pages/Post/ContentGenerator.cs
@@ -1,11 +1,51 @@
+using Azure;
+using Azure.AI.OpenAI;
+using static System.Environment;
+
 namespace CowPress.Pages.Post;
 
 public class ContentGenerator
 {
-    public async Task<string> GenerateContent(string subject) {
-        // TODO Implement AI
+    
+    public readonly OpenAIClient _openAIClient;
 
-        return await Task.FromResult("Lorem Ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue");
+    public ContentGenerator(OpenAIClient openAIClient)
+    {
+        _openAIClient = openAIClient;
+    }
+    
+    public async Task<string> GenerateContent(string subject) {
+
+        OpenAIClient client = new(new Uri("https://dev-week-2023.openai.azure.com/"), new AzureKeyCredential("7b68f68e0d7943fbb990eb83e4c02e1d"));
+
+        var chatCompletionsOptions = new ChatCompletionsOptions()
+        {
+            DeploymentName = "gpt-4", //This must match the custom deployment name you chose for your model
+            Messages =
+            {
+                new ChatMessage(ChatRole.System, $"Create a blog post in 250 characters at most"),
+                new ChatMessage(ChatRole.User, subject),
+            },
+            MaxTokens = 250
+        };
+
+        Response<ChatCompletions> response = client.GetChatCompletions(chatCompletionsOptions);
+
+        Console.WriteLine(response.Value.Choices[0].Message.Content);
+
+        return response.Value.Choices[0].Message.Content;
+
+
+        /*
+        var request = new CompletionsOptions()
+        {
+            DeploymentName = "gpt-4", // assumes a matching model deployment or model name
+            Prompts = { "Hello, world!" },
+        };
+        var response = await _openAIClient.GetCompletionsAsync(request);
+
+        return response.Value.Choices.FirstOrDefault().Text;
+        */
     }
 
     public async Task<IReadOnlyList<float>> GenerateEmbedding(string text) {
--- CowPress/Program.cs
+++ CowPress/Program.cs
@@ -3,6 +3,8 @@ using Microsoft.EntityFrameworkCore;
 using CowPress.Data;
 using CowPress.Utils;
 using CowPress.Pages.Post;
+using Azure.AI.OpenAI;
+using Azure;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -27,6 +29,10 @@ builder.Services.Configure<IdentityOptions>(options =>
 });
 
 // TODO add ai service
+
+builder.Services.AddSingleton(new OpenAIClient(
+        new Uri("https://dev-week-2023.openai.azure.com/"),
+        new AzureKeyCredential("7b68f68e0d7943fbb990eb83e4c02e1d")));
 
 builder.Services.AddTransient<ContentGenerator>();
 builder.Services.AddTransient<RelatedBlogPosts>();
